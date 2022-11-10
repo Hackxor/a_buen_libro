d90d6e516e97118e474b9c604aea84ed0d5bd700
Hackxor <d3f4ltred@gmail.com>
2022-11-10 00:52:16 -0600

    command 3

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const Sequelize = require('sequelize');
-const {DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} = require('./produccion');
 
 
-module.exports = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-    DB_HOST,
-    DB_PORT,
+
+module.exports = new Sequelize('railway', 'root', 'RIyzYAeDSBh0taUPOHhv', {
+    host : 'containers-us-west-91.railway.app',
+    port : '5687',
     dialect : 'mysql',
     pool : {
         max : 5,
--- config/produccion.js
+++ config/produccion.js
@@ -6,4 +6,4 @@
  const DB_NAME = process.env.DB_NAME || 'libreria'
  const DB_PORT = process.env.DB_PORT || 3306
 
-module.exports = {PORTS,DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER}
+module.exports = {PORTS,DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER} ;
